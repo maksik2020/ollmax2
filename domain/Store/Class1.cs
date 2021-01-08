842f367af3fc202114b459bfe0f29b9fb38c73b5
maksik <maksikoro@mail.ru>
2021-01-09 01:25:12 +0300

    Создание web проекта

--- domain/Store/Class1.cs
+++ domain/Store/Class1.cs
@@ -2,7 +2,14 @@
 
 namespace Store
 {
-    public class Class1
+    public class Book
     {
+        public int Id { get; }
+        public string Title { get; }
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
     }
 }
